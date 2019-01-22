44719e654b184ed8a1512669eb1b1e4312ef45a1
Tobias Hellmann <Tobi823@users.noreply.github.com>
2019-01-22 12:44:59 +0100

    Implement "restart after too many unlock attempts" - improve "wipe after too many unlock attempts" - add preference-keys into keys.xml - add one SwitchPreference for "restart after too many unlock attempts" in preferences.xml - add two EditTextPreference for configure when the device should be wipe or restarted - add logic to aquire device owner permission

--- app/src/main/java/com/davidshewitt/admincontrol/ControlDeviceAdminReceiver.java
+++ app/src/main/java/com/davidshewitt/admincontrol/ControlDeviceAdminReceiver.java
@@ -18,21 +18,78 @@
 
 package com.davidshewitt.admincontrol;
 
+import android.annotation.SuppressLint;
 import android.app.admin.DeviceAdminReceiver;
-//import android.content.Context;
-//import android.content.Intent;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.util.Objects;
+
+import static com.davidshewitt.admincontrol.ControlYourDeviceActivity.*;
+
 
 public class ControlDeviceAdminReceiver extends DeviceAdminReceiver {
+    public static final int DEFAULT_MAX_UNLOCK_ATTEMPTS_BEFORE_RESTART = 10;
 
-    /*
     @Override
     public void onPasswordFailed(Context context, Intent intent) {
-
+        advancedBruteForceProtection(context);
     }
 
-    @Override
-    public void onPasswordSucceeded(Context context, Intent intent) {
-
+    /**
+     * Get the ComponentName for this class. This is necessary for a lot of DevicePolicyManager methods.
+     * @return
+     */
+    protected static ComponentName getComponentName() {
+        Class<ControlDeviceAdminReceiver> clazz = ControlDeviceAdminReceiver.class;
+        return new ComponentName(clazz.getPackage().getName(), clazz.getName());
     }
-    */
+
+    /**
+     * If the device has too many failed unlock attempts, then restart it (depending on the configuration
+     * given by the user.
+     * @param context
+     */
+    @SuppressLint("NewApi") // because Lint does not detect my first if-condition checking the API
+    private void advancedBruteForceProtection(Context context) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            Log.i(LOG_TAG, "This android version doesn't support DevicePolicyManager.reboot()");
+            return;
+        }
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        if (!preferences.getBoolean(context.getString(R.string.key_restart_after_too_many_unlock_attempts), false)) {
+            Log.i(LOG_TAG, "Advanced unlock brute-force protection is disabled");
+            return;
+        }
+
+        DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+        Objects.requireNonNull(dpm);
+
+        String key = context.getString(R.string.key_max_unlock_attempts_before_restart);
+        String _maxAttempts = preferences.getString(key, null);
+        int maxAttempts = NumberUtils.toInt(_maxAttempts, DEFAULT_MAX_UNLOCK_ATTEMPTS_BEFORE_RESTART);
+
+        int currentFailedPasswordAttempts = dpm.getCurrentFailedPasswordAttempts();
+        if (currentFailedPasswordAttempts <= maxAttempts) {
+            Log.i(LOG_TAG, "current failed password attempts: " + currentFailedPasswordAttempts);
+            Log.i(LOG_TAG, "max failed attempts for reboot: " + maxAttempts);
+            return;
+        }
+
+        try {
+            Log.i(LOG_TAG, "Execute an reboot.");
+            dpm.reboot(getComponentName());
+        } catch (IllegalStateException e) {
+            Log.e(LOG_TAG, "Cant reboot device due to an ongoing call.", e);
+        }
+    }
 }
--- app/src/main/java/com/davidshewitt/admincontrol/ControlYourDeviceActivity.java
+++ app/src/main/java/com/davidshewitt/admincontrol/ControlYourDeviceActivity.java
@@ -23,13 +23,21 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.io.IOException;
 
 /**
  * A {@link PreferenceActivity} that lets the user control advanced security settings on their
@@ -37,6 +45,9 @@ import android.util.Log;
  */
 public class ControlYourDeviceActivity extends AppCompatPreferenceActivity {
     public static String LOG_TAG = "com.davidshewitt.admincontrol";
+    public static final int DISABLE_DEVICE_UNLOCK_PROTECTION = 0;
+    public static final int DEFAULT_MAX_UNLOCK_ATTEMPTS_BEFORE_WIPE = 10;
+
 
     private static final int ACTIVATION_REQUEST = 1;
 
@@ -71,37 +82,85 @@ public class ControlYourDeviceActivity extends AppCompatPreferenceActivity {
         }
     };
 
-    public static final int MAX_TRIES_FOR_DEVICE_UNLOCK_PASSWORDS = 10;
-    public static final int DISABLE_DEVICE_UNLOCK_PROTECTION = 0;
-    private static final Preference.OnPreferenceChangeListener sWipeAfterIncorrectPasswordListener =
+    /**
+     * Listener for the wipe_after_too_many_unlock_attempts SwitchPreference
+     */
+    private static final Preference.OnPreferenceChangeListener sWipeAfterTooManyUnlockAttemptsListener =
             new Preference.OnPreferenceChangeListener() {
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object o) {
-            ControlYourDeviceActivity mainPrefActivity =
-                    ((ControlYourDeviceActivity) preference.getContext());
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object o) {
+                    ControlYourDeviceActivity mainPrefActivity =
+                            ((ControlYourDeviceActivity) preference.getContext());
 
-            DevicePolicyManager dpm = mainPrefActivity.getDPM();
-            ComponentName deviceOwnerComponent = mainPrefActivity.getDeviceOwnerComponent();
-            int maxTries = (Boolean) o ? MAX_TRIES_FOR_DEVICE_UNLOCK_PASSWORDS : DISABLE_DEVICE_UNLOCK_PROTECTION;
+                    if (!mainPrefActivity.hasDeviceAdmin()) {
+                        mainPrefActivity.showPermissionExplanation();
+                        return false;
+                    }
 
-            if (mainPrefActivity.hasDeviceAdmin()) {
-                try {
-                    dpm.setMaximumFailedPasswordsForWipe(deviceOwnerComponent, maxTries);
+                    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mainPrefActivity);
+                    String key = mainPrefActivity.getString(R.string.key_max_unlock_attempts_before_wipe);
+                    String _maxTries = preferences.getString(key, null);
+                    int maxTries = NumberUtils.toInt(_maxTries, DEFAULT_MAX_UNLOCK_ATTEMPTS_BEFORE_WIPE);
+                    mainPrefActivity.setMaximumFailedPasswordsForWipe((Boolean) o, maxTries);
                     return true;
-                } catch (SecurityException e) {
-                    Log.e(LOG_TAG, "DevicePolicyManager.setMaximumFailedPasswordsForWipe failed", e);
-                    new AlertDialog.Builder(mainPrefActivity)
-                            .setTitle(R.string.dialog_error_title)
-                            .setMessage(R.string.please_reinstall)
-                            .setPositiveButton(R.string.dialog_ok, null)
-                            .show();
-                    return false;
                 }
-            }
-            mainPrefActivity.showPermissionExplanation();
-            return false;
-        }
-    };
+            };
+
+    /**
+     * Listener for the max_unlock_attempts_for_wipe EditTextPreference
+     */
+    private static final Preference.OnPreferenceChangeListener sMaxUnlockAttemptsBeforeWipeListener =
+            new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object o) {
+                    ControlYourDeviceActivity mainPrefActivity =
+                            ((ControlYourDeviceActivity) preference.getContext());
+
+                    if (!mainPrefActivity.hasDeviceAdmin()) {
+                        mainPrefActivity.showPermissionExplanation();
+                        return false;
+                    }
+
+                    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mainPrefActivity);
+                    String key = mainPrefActivity.getString(R.string.key_wipe_after_too_many_unlock_attempts);
+                    boolean enabled = preferences.getBoolean(key, false);
+                    int maxTries = NumberUtils.toInt((String) o, DEFAULT_MAX_UNLOCK_ATTEMPTS_BEFORE_WIPE);
+                    mainPrefActivity.setMaximumFailedPasswordsForWipe(enabled, maxTries);
+                    return true;
+                }
+            };
+
+    /**
+     * Listener for the restart_after_too_many_unlock_attempts SwitchPreference
+     */
+    private static final Preference.OnPreferenceChangeListener sRestartAfterTooManyUnlockAttemptsListener =
+            new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object o) {
+                    ControlYourDeviceActivity mainPrefActivity =
+                            ((ControlYourDeviceActivity) preference.getContext());
+
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+                        mainPrefActivity.showUnsupportedAndroidVersion();
+                        return false;
+                    }
+
+                    DevicePolicyManager dpm = mainPrefActivity.getDPM();
+                    ComponentName deviceOwnerComponent = mainPrefActivity.getDeviceOwnerComponent();
+
+                    if (!mainPrefActivity.hasDeviceAdmin()) {
+                        mainPrefActivity.promptDeviceAdmin();
+                        return false;
+                    }
+
+                    if (!dpm.isDeviceOwnerApp(deviceOwnerComponent.getPackageName())) {
+                        mainPrefActivity.showDeviceOwnershipExplanation();
+                        return false;
+                    }
+
+                    return true;
+                }
+            };
 
     /**
      * Main preference fragment.
@@ -113,10 +172,34 @@ public class ControlYourDeviceActivity extends AppCompatPreferenceActivity {
             addPreferencesFromResource(R.xml.preferences);
             findPreference("disableFingerprintLockscreen")
                     .setOnPreferenceChangeListener(sFingerprintLockscreenListener);
-            findPreference("wipeAfterTooManyIncorrectDeviceUnlockPasswordAttempts")
-                    .setOnPreferenceChangeListener(sWipeAfterIncorrectPasswordListener);
+            findPreference(getString(R.string.key_wipe_after_too_many_unlock_attempts))
+                    .setOnPreferenceChangeListener(sWipeAfterTooManyUnlockAttemptsListener);
+            findPreference(getString(R.string.key_max_unlock_attempts_before_wipe))
+                    .setOnPreferenceChangeListener(sMaxUnlockAttemptsBeforeWipeListener);
+            findPreference(getString(R.string.key_restart_after_too_many_unlock_attempts))
+                    .setOnPreferenceChangeListener(sRestartAfterTooManyUnlockAttemptsListener);
         }
 
+    }
+
+    /**
+     * Calls the method setMaximumFailedPasswordsForWipe from DevicePolicyManager wit the correct
+     * parameter.
+     * if @param enabled == false -> setMaximumFailedPasswordsForWipe(0),
+     * else setMaximumFailedPasswordsForWipe(@param maxTries)
+     * @param enabled
+     * @param maxTries
+     */
+    private void setMaximumFailedPasswordsForWipe(boolean enabled, int maxTries) {
+        int tries = enabled ? maxTries : DISABLE_DEVICE_UNLOCK_PROTECTION;
+        try {
+            DevicePolicyManager dpm = getDPM();
+            ComponentName deviceOwnerComponent = getDeviceOwnerComponent();
+            Log.i(LOG_TAG, "Set setMaximumFailedPasswordsForWipe to " + tries);
+            dpm.setMaximumFailedPasswordsForWipe(deviceOwnerComponent, tries);
+        } catch (SecurityException e) {
+            Log.e(LOG_TAG, "DevicePolicyManager.setMaximumFailedPasswordsForWipe failed", e);
+        }
     }
 
     /**
@@ -161,6 +244,21 @@ public class ControlYourDeviceActivity extends AppCompatPreferenceActivity {
             startActivityForResult(requestDeviceAdminIntent, ACTIVATION_REQUEST);
     }
 
+    private void promptDeviceOwnership() {
+        try {
+            Process process = Runtime.getRuntime().exec(
+                    new String[] { "su", "-c", "dpm set-device-owner " + mDeviceOwnerComponent.flattenToString()});
+            process.waitFor();
+        } catch (InterruptedException|IOException ex) {
+            Log.w(LOG_TAG, "Cannot set the device-owner with a root command - maybe the device is not rooted or the user didn't give root permission to the app.", ex);
+        }
+
+        if (!mDPM.isDeviceOwnerApp(mDeviceOwnerComponent.getPackageName())) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/linux-colonel/AdminControl")));
+        }
+    }
+
+
     /**
      * Set up the {@link android.app.ActionBar}, if the API is available.
      */
@@ -185,6 +283,28 @@ public class ControlYourDeviceActivity extends AppCompatPreferenceActivity {
                        promptDeviceAdmin();
                     }
                     })
+                .setNegativeButton(R.string.dialog_cancel, null)
+                .show();
+    }
+
+    private void showUnsupportedAndroidVersion() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.dialog_error_title)
+                .setMessage("This feature is only supported for Android 7.0 or above")
+                .setPositiveButton(R.string.dialog_ok, null)
+                .show();
+    }
+
+    private void showDeviceOwnershipExplanation() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.dialog_explain_permissions_title)
+                .setMessage(R.string.dialog_explain_device_owner_permission)
+                .setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        promptDeviceOwnership();
+                    }
+                })
                 .setNegativeButton(R.string.dialog_cancel, null)
                 .show();
     }
